0e35f0ed666e8934348651b09a1adb70ac1e1e1c
ChlorUpload <chlor.upload@gmail.com>
2022-07-09 19:07:28 +0900

    Segregate process_result into train set and validation set

--- utils/process.py
+++ utils/process.py
@@ -6,9 +6,16 @@ from anime_face_detect import detect
 
 countries = ["mondstadt", "liyue", "inazuma"]
 
+
 def process():
     if not os.path.exists('process_result'):
         os.mkdir('process_result')
+
+    if not os.path.exists('process_result/train'):
+        os.mkdir('process_result/train')
+
+    if not os.path.exists('process_result/valid'):
+        os.mkdir('process_result/valid')
 
     for country in countries:
         characters = os.listdir(country)
@@ -20,20 +27,22 @@ def process():
                 continue
             print(f"processing character: {character}")
             images = os.listdir(character_dir)
-            for image in images:
+            num_trains = int(len(images) * 0.8)
+            for img_idx, image in enumerate(images):
                 img, faces = detect(f"{country}/{character}/{image}")
-
                 image_path = pathlib.Path(image)
                 stem = image_path.stem
                 suffix = image_path.suffix
-                
+                filter = 'train' if img_idx < num_trains else 'valid'
+
                 for idx, face in enumerate(faces):
                     x, y, w, h = face
                     cropped = img[y:y+h, x:x+w]
                     resized = cv2.resize(cropped, (128, 128))
-                    cv2.imwrite(f'process_result/{country}-{character}-{stem}-{idx}{suffix}', resized)
-                    
-                
+
+                    cv2.imwrite(
+                        f'process_result/{filter}/{country}-{character}-{stem}-{idx}{suffix}', resized)
+
 
 if __name__ == "__main__":
     process()
